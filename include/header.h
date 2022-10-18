a97683b8c698fb7190cd66338b3971c6529dd5f2
eliteabhi <abhirangarajan2003@gmail.com>
2022-10-18 11:06:56 -0500

    Refactor/Fixed functions

--- include/header.h
+++ include/header.h
@@ -6,7 +6,7 @@
 
 #define BMP_SCL A5
 #define BMP_SDA A4 
-#define SEALEVELPRESSURE_HPA (1013.25)
+#define SEALEVELPRESSURE_HPA ( 1013.25 )
 
 #define pinCS 10
 #define pinMOSI 13
@@ -15,17 +15,16 @@
 #define AccelRange 16
 #define GyroRange 2000
 
-bool bmp_exists, mpu_exists, sd_exists;
 byte AFS_SEL, GFS_SEL;
 short ALSB_Sensitivity, GLSB_Sensitivity;
 
-File myFile = SD.open("Raw_V05.csv", FILE_WRITE);
+File myFile = SD.open( "Raw_V05.csv", FILE_WRITE );
 
 Adafruit_BMP3XX bmp;
 
-int Set_Accel_Range(byte range) {
+int Set_Accel_Range( byte range ) {
 
-  switch (AccelRange) {
+  switch ( AccelRange ) {
 
     case 2:
       AFS_SEL = 0x00;
@@ -48,7 +47,7 @@ int Set_Accel_Range(byte range) {
     break;
 
     default:
-      Serial.println("Must input 2, 4, 8, or 16g for MPU6050 accelerometer range! Exiting...");
+      Serial.println( "Must input 2, 4, 8, or 16g for MPU6050 accelerometer range! Exiting..." );
       return 1;
   }
 
@@ -56,9 +55,9 @@ int Set_Accel_Range(byte range) {
 
 }
 
-int Set_Gyro_Range(short range) {
+int Set_Gyro_Range( short range ) {
 
-  switch (GyroRange) {
+  switch ( GyroRange ) {
 
     case 250:
       GFS_SEL = 0x00;
@@ -81,7 +80,7 @@ int Set_Gyro_Range(short range) {
       break;
 
     default:
-      Serial.println("Must input 250, 500, 1000, or 2000 deg/s for MPU6050 gyroscope range! Exiting...");
+      Serial.println( "Must input 250, 500, 1000, or 2000 deg/s for MPU6050 gyroscope range! Exiting..." );
       return 0;
   }
 
@@ -91,28 +90,28 @@ int Set_Gyro_Range(short range) {
 
 Vector<int> Get_Raw_Accel() {
 
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
+  Wire.beginTransmission( MPU );
+  Wire.write( 0x3B ); // Start with register 0x3B ( ACCEL_XOUT_H )
+  Wire.endTransmission( false );
+  Wire.requestFrom( MPU, 6, true ); // Read 6 registers total, each axis value is stored in 2 registers
 
   Vector<int> result;
 
-  result.push_back((Wire.read() << 8 | Wire.read()) * 1000); // Raw X-axis value
-  result.push_back((Wire.read() << 8 | Wire.read()) * 1000); // Raw Y-axis value
-  result.push_back((Wire.read() << 8 | Wire.read()) * 1000); // Raw Z-axis value
+  result.push_back( ( Wire.read() << 8 | Wire.read() ) * 1000 ); // Raw X-axis value
+  result.push_back( ( Wire.read() << 8 | Wire.read() ) * 1000 ); // Raw Y-axis value
+  result.push_back( ( Wire.read() << 8 | Wire.read() ) * 1000 ); // Raw Z-axis value
 
   return result;
 
 }
 
-Vector<int> Normalized_Accel(const Vector<int>& raw_accel) {
+Vector<int> Normalized_Accel( const Vector<int>& raw_accel ) {
   
   Vector<int> normalized_accel;
 
-  normalized_accel.push_back(raw_accel.at(0) / ALSB_Sensitivity); 
-  normalized_accel.push_back(raw_accel.at(1) / ALSB_Sensitivity);
-  normalized_accel.push_back(raw_accel.at(2) / ALSB_Sensitivity);
+  normalized_accel.push_back( raw_accel.at( 0 ) / ALSB_Sensitivity ); 
+  normalized_accel.push_back( raw_accel.at( 1 ) / ALSB_Sensitivity );
+  normalized_accel.push_back( raw_accel.at( 2 ) / ALSB_Sensitivity );
 
   return normalized_accel;
 
@@ -120,28 +119,28 @@ Vector<int> Normalized_Accel(const Vector<int>& raw_accel) {
 
 Vector<int> Get_Raw_Gyro() {
 
-  Wire.beginTransmission(MPU);
-  Wire.write(0x43); // Gyro data first register address 0x43
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 4 registers total, each axis value is stored in 2 registers
+  Wire.beginTransmission( MPU );
+  Wire.write( 0x43 ); // Gyro data first register address 0x43
+  Wire.endTransmission( false );
+  Wire.requestFrom( MPU, 6, true ); // Read 4 registers total, each axis value is stored in 2 registers
 
   Vector<int> raw_gyro;
 
-  raw_gyro.push_back((Wire.read() << 8 | Wire.read()) * 10000);
-  raw_gyro.push_back((Wire.read() << 8 | Wire.read()) * 10000);
-  raw_gyro.push_back((Wire.read() << 8 | Wire.read()) * 10000);
+  raw_gyro.push_back( ( Wire.read() << 8 | Wire.read() ) * 10000 );
+  raw_gyro.push_back( ( Wire.read() << 8 | Wire.read() ) * 10000 );
+  raw_gyro.push_back( ( Wire.read() << 8 | Wire.read() ) * 10000 );
 
   return raw_gyro;
 
 }
 
-Vector<int> Normalized_Gyro(const Vector<int>& raw_gyro) {
+Vector<int> Normalized_Gyro( const Vector<int>& raw_gyro ) {
 
   Vector<int> normalized_gyro;
 
-  normalized_gyro.push_back(raw_gyro.at(0) / GLSB_Sensitivity);
-  normalized_gyro.push_back(raw_gyro.at(1) / GLSB_Sensitivity);
-  normalized_gyro.push_back(raw_gyro.at(2) / GLSB_Sensitivity);
+  normalized_gyro.push_back( raw_gyro.at( 0 ) / GLSB_Sensitivity );
+  normalized_gyro.push_back( raw_gyro.at( 1 ) / GLSB_Sensitivity );
+  normalized_gyro.push_back( raw_gyro.at( 2 ) / GLSB_Sensitivity );
 
   return normalized_gyro;
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,122 +9,128 @@ void setup() {
   // Find hexadecimal representation of accelerometer range based on decimal global variable AccelRange defined above // 
   // Find decimal representation of LSB Sensitivity based on decimal global variable AccelRange defined above // 
 
-  Serial.begin(115200); //Changed to higher rate 4/21/22
+  Serial.begin( 115200 ); //Changed to higher rate 4/21/22
 
-  while (!Serial);
+  while ( !Serial );
 
-  if (!bmp.begin_I2C()) { // hardware I2C mode, can pass in address & alt Wire
-    Serial.println("Could not find a valid BMP3 sensor, check wiring!\n");
-    bmp_exists = false;
+  while ( !bmp.begin_I2C() ) { // hardware I2C mode, can pass in address & alt Wire
+
+    Serial.println( "Could not find a valid BMP3 sensor, check wiring!\n" );
+    delay( 2000 );
+    
   }
 
-  Wire.beginTransmission(MPU);
-  mpu_exists = (Wire.endTransmission() == 0) ? true : false;
-  if (!mpu_exists) {
-    Serial.println("Could not find MPU\n");
+  Wire.beginTransmission( MPU );
+  while ( ( Wire.endTransmission() != 0 ) ) {
+
+    Serial.println( "Could not find MPU\n" );
+    delay( 2000 );
+    
   }
 
   // Set up oversampling and filter initialization
 
-  if (bmp_exists) {
-    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-    bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-    bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+  if ( bmp_exists ) {
+
+    bmp.setTemperatureOversampling( BMP3_OVERSAMPLING_8X );
+    bmp.setPressureOversampling( BMP3_OVERSAMPLING_4X );
+    bmp.setIIRFilterCoeff( BMP3_IIR_FILTER_COEFF_3 );
+    bmp.setOutputDataRate( BMP3_ODR_50_HZ );
+
   }
 
-  pinMode(pinCS, OUTPUT);
-  pinMode(pinMOSI, OUTPUT);
+  pinMode( pinCS, OUTPUT );
+  pinMode( pinMOSI, OUTPUT );
 
-  while (Set_Accel_Range(AccelRange) != 0) {
+  while ( Set_Accel_Range( AccelRange ) != 0 ) {
     
-    Serial.println("Please Fix Accel Range");
-    delay(2000);
+    Serial.println( "Please Fix Accel Range" );
+    delay( 2000 );
 
     }
   
-  while (Set_Gyro_Range(GyroRange) != 0) {
+  while ( Set_Gyro_Range( GyroRange ) != 0 ) {
 
-    Serial.println("Please Fix Gyro Range");
-    delay(2000);
+    Serial.println( "Please Fix Gyro Range" );
+    delay( 2000 );
 
-  };
+  }
 
   // Find hexadecimal representation of gyroscope range based on decimal global variable GyroRange defined above // 
   // Find decimal representation of LSB Sensitivity based on decimal global variable GyroRange defined above // 
 
   Wire.begin();                      // Initialize comunication
-  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
-  Wire.write(0x6B);                  // Talk to the register 6B
-  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
-  Wire.endTransmission(true);        //end the transmission
+  Wire.beginTransmission( MPU );       // Start communication with MPU6050 // MPU=0x68
+  Wire.write( 0x6B );                  // Talk to the register 6B
+  Wire.write( 0x00 );                  // Make reset - place a 0 into the 6B register
+  Wire.endTransmission( true );        //end the transmission
   
-  // Configure Accelerometer Sensitivity - Full Scale Range (default +/- 2g)
-  Wire.beginTransmission(MPU);
-  Wire.write(0x1C);                  //Talk to the ACCEL_CONFIG register (1C hex)
-  Wire.write(AFS_SEL);               //Set the register bits as 00010000 (+/- 8g full scale range)
-  Wire.endTransmission(true);
+  // Configure Accelerometer Sensitivity - Full Scale Range ( default +/- 2g )
+  Wire.beginTransmission( MPU );
+  Wire.write( 0x1C );                  //Talk to the ACCEL_CONFIG register ( 1C hex )
+  Wire.write( AFS_SEL );               //Set the register bits as 00010000 ( +/- 8g full scale range )
+  Wire.endTransmission( true );
   
-  // Configure Gyro Sensitivity - Full Scale Range (default +/- 250deg/s)
-  Wire.beginTransmission(MPU);
-  Wire.write(0x1B);                   // Talk to the GYRO_CONFIG register (1B hex)
-  Wire.write(GFS_SEL);                // Set the register bits as 00010000 (1000deg/s full scale)
-  Wire.endTransmission(true);
+  // Configure Gyro Sensitivity - Full Scale Range ( default +/- 250deg/s )
+  Wire.beginTransmission( MPU );
+  Wire.write( 0x1B );                   // Talk to the GYRO_CONFIG register ( 1B hex )
+  Wire.write( GFS_SEL );                // Set the register bits as 00010000 ( 1000deg/s full scale )
+  Wire.endTransmission( true );
 
-  myFile.print("Time (seconds),Raw Ax (g),Ax (g),Raw Ay (g),Ay (g),Raw Az (g),Az (g),Raw Gx (deg/s),Gx (deg/s),Raw Gy (deg/s),Gy (deg/s),Raw Gz (deg/s),Gz (deg/s), Temperature (*C), Pressure (kpA), Altitude (m)");
-  myFile.print("Time,RAx,Ax,RAy,Ay,RAz,Az,RGx,Gx,RGy,Gy,RGz,Gz, Temp (*C), P (kPa), Alt (m)");
+  myFile.print( "Time ( seconds ),Raw Ax ( g ),Ax ( g ),Raw Ay ( g ),Ay ( g ),Raw Az ( g ),Az ( g ),Raw Gx ( deg/s ),Gx ( deg/s ),Raw Gy ( deg/s ),Gy ( deg/s ),Raw Gz ( deg/s ),Gz ( deg/s ), Temperature ( *C ), Pressure ( kpA ), Altitude ( m )" );
+  myFile.print( "Time,RAx,Ax,RAy,Ay,RAz,Az,RGx,Gx,RGy,Gy,RGz,Gz, Temp ( *C ), P ( kPa ), Alt ( m )" );
  
   myFile.println();
   myFile.close();
 
   Serial.end();
 
-  delay(20);
+  delay( 20 );
 
 }
 
 void loop() {
 
-  Serial.begin(115200);
-  while (!Serial);
+  Serial.begin( 115200 );
+  while ( !Serial );
   
   // === Read acceleromter data === //
   
   Vector<int> raw_accel = Get_Raw_Accel();
-  Vector<int> accel = Normalized_Accel(raw_accel);
+  Vector<int> accel = Normalized_Accel( raw_accel );
 
   // === Read gyroscope data === //
 
   const Vector<int> raw_gyro = Get_Raw_Gyro();
-  const Vector<int> gyro = Normalized_Gyro(raw_gyro);
+  const Vector<int> gyro = Normalized_Gyro( raw_gyro );
  
   // Print the values on the serial monitor
 
   char c = ',';
 
-  Serial.println("Time (S): " + String(millis() / 1000.0));
+  Serial.println( "Time ( S ): " + String( millis() / 1000.0 ) );
 
-  Serial.println("Raw Acceleration (X, Y, Z): " + String(raw_accel.at(0) / 1000.0, 2) + c + String(raw_accel.at(1) / 1000.0, 2) + c + String(raw_accel.at(0)  / 1000.0, 2)); // Arduino is really dumb
+  Serial.println( "Raw Acceleration ( X, Y, Z ): " + String( raw_accel.at( 0 ) / 1000.0, 2 ) + c + String( raw_accel.at( 1 ) / 1000.0, 2 ) + c + String( raw_accel.at( 0 )  / 1000.0, 2 ) ); // Arduino is really dumb
   
-  Serial.println("Normalized Acceleration (X, Y, Z): " + String(accel.at(0)  / 1000.0, 2) + c + String(accel.at(1) / 1000.0, 2) + c + String(accel.at(2) / 1000.0, 2)); // Arduino is really dumb again
+  Serial.println( "Normalized Acceleration ( X, Y, Z ): " + String( accel.at( 0 )  / 1000.0, 2 ) + c + String( accel.at( 1 ) / 1000.0, 2 ) + c + String( accel.at( 2 ) / 1000.0, 2 ) ); // Arduino is really dumb again
   
-  Serial.println("Raw GyroRange (X, Y, Z): " + String(raw_gyro.at(0) / 1000.0, 2) + c + String(raw_gyro.at(1) / 1000.0, 2) + c + String(raw_gyro.at(2) / 1000.0, 2)); // Arduino is really dumb again,...again
+  Serial.println( "Raw GyroRange ( X, Y, Z ): " + String( raw_gyro.at( 0 ) / 1000.0, 2 ) + c + String( raw_gyro.at( 1 ) / 1000.0, 2 ) + c + String( raw_gyro.at( 2 ) / 1000.0, 2 ) ); // Arduino is really dumb again,...again
 
-  Serial.println("Normalized Gyro Range (X, Y, Z): " + String(gyro.at(0) / 1000.0, 2) + c + String(gyro.at(1) / 1000.0, 2) + c + String(gyro.at(2) / 1000.0, 2) + '\n'); // Arduino is really dumb again,...again, and again
+  Serial.println( "Normalized Gyro Range ( X, Y, Z ): " + String( gyro.at( 0 ) / 1000.0, 2 ) + c + String( gyro.at( 1 ) / 1000.0, 2 ) + c + String( gyro.at( 2 ) / 1000.0, 2 ) + '\n' ); // Arduino is really dumb again,...again, and again
 
-  Serial.println("Now reading BMP390...");
+  Serial.println( "Now reading BMP390..." );
   
-  Serial.println("Tempurature (C): " + String(bmp.temperature));
+  Serial.println( "Tempurature ( C ): " + String( bmp.temperature ) );
   
-  Serial.println("Pressure (kPa): " + String(bmp.pressure / 1000.0));
+  Serial.println( "Pressure ( kPa ): " + String( bmp.pressure / 1000.0 ) );
 
-  Serial.println("Altitude (m): " + String(bmp.readAltitude(SEALEVELPRESSURE_HPA)) + '\n');
+  Serial.println( "Altitude ( m ): " + String( bmp.readAltitude( SEALEVELPRESSURE_HPA ) ) + '\n' );
 
   Serial.end();
 
-  // myFile = SD.open("Raw_V05.csv", FILE_WRITE);
-  // myFile.println(String(bmp.temperature) + c + String(bmp.pressure / 1000.0) + c + String(bmp.readAltitude(SEALEVELPRESSURE_HPA)) + '\n');
+  // myFile = SD.open( "Raw_V05.csv", FILE_WRITE );
+  // myFile.println( String( bmp.temperature ) + c + String( bmp.pressure / 1000.0 ) + c + String( bmp.readAltitude( SEALEVELPRESSURE_HPA ) ) + '\n' );
   // myFile.close();
 
-  delay(2000);
+  delay( 2000 );
 }

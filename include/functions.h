d058d269548179f85e2072e4b46589c0468f1f7c
eliteabhi <abhirangarajan2003@gmail.com>
2023-02-08 12:27:32 -0600

    Converted Print_All_Values to use a string and print once

--- include/functions.h
+++ include/functions.h
@@ -14,23 +14,19 @@ void Print_All_Values() { // Print the values on the serial monitor
   const Vector<int> normalized_gyro = Normalized_Gyro( raw_gyro );
 
   // Print all relevent values
-  Serial.println( "Time ( S ): " + String( millis() / 1000.0 ) );
 
-  Serial.println( "Raw Acceleration ( X, Y, Z ): " + String( raw_accel.at( 0 ) / 1000.0, 2 ) + ',' + String( raw_accel.at( 1 ) / 1000.0, 2 ) + ',' + String( raw_accel.at( 0 )  / 1000.0, 2 ) ); // Arduino is really dumb
-  
-  Serial.println( "Normalized Acceleration ( X, Y, Z ): " + String( normalized_accel.at( 0 )  / 1000.0, 2 ) + ',' + String( normalized_accel.at( 1 ) / 1000.0, 2 ) + ',' + String( normalized_accel.at( 2 ) / 1000.0, 2 ) ); // Arduino is really dumb again
-  
-  Serial.println( "Raw GyroRange ( X, Y, Z ): " + String( raw_gyro.at( 0 ) / 1000.0, 2 ) + ',' + String( raw_gyro.at( 1 ) / 1000.0, 2 ) + ',' + String( raw_gyro.at( 2 ) / 1000.0, 2 ) ); // Arduino is really dumb again,...again
+  String output = "";
+  output.append( "Time ( S ): " + String( millis() / 1000.0 ) + "\n" );
+  output.append( "Raw Acceleration ( X, Y, Z ): " + String( raw_accel.at( 0 ) / 1000.0, 2 ) + ',' + String( raw_accel.at( 1 ) / 1000.0, 2 ) + ',' + String( raw_accel.at( 0 )  / 1000.0, 2 ) + "\n" );
+  output.append( "Normalized Acceleration ( X, Y, Z ): " + String( normalized_accel.at( 0 )  / 1000.0, 2 ) + ',' + String( normalized_accel.at( 1 ) / 1000.0, 2 ) + ',' + String( normalized_accel.at( 2 ) / 1000.0, 2 ) + "\n" );
+  output.append( "Raw GyroRange ( X, Y, Z ): " + String( raw_gyro.at( 0 ) / 1000.0, 2 ) + ',' + String( raw_gyro.at( 1 ) / 1000.0, 2 ) + ',' + String( raw_gyro.at( 2 ) / 1000.0, 2 ) + "\n" ); // Arduino is really dumb again,...againauto
+  output.append( "Normalized Gyro Range ( X, Y, Z ): " + String( normalized_gyro.at( 0 ) / 1000.0, 2 ) + ',' + String( normalized_gyro.at( 1 ) / 1000.0, 2 ) + ',' + String( normalized_gyro.at( 2 ) / 1000.0, 2 ) + '\n' );
+  output.append( "\n\n Now reading BMP390...\n" );
+  output.append( "Tempurature ( C ): " + String( bmp.temperature ) + "\n" );
+  output.append( "Pressure ( kPa ): " + String( bmp.pressure / 1000.0 ) + "\n" );
+  output.append( "Altitude ( m ): " + String( bmp.readAltitude( SEALEVELPRESSURE_HPA ) ) + '\n' );
 
-  Serial.println( "Normalized Gyro Range ( X, Y, Z ): " + String( normalized_gyro.at( 0 ) / 1000.0, 2 ) + ',' + String( normalized_gyro.at( 1 ) / 1000.0, 2 ) + ',' + String( normalized_gyro.at( 2 ) / 1000.0, 2 ) + '\n' ); // Arduino is really dumb again,...again, and again
-
-  Serial.println( "Now reading BMP390..." );
-  
-  Serial.println( "Tempurature ( C ): " + String( bmp.temperature ) );
-  
-  Serial.println( "Pressure ( kPa ): " + String( bmp.pressure / 1000.0 ) );
-
-  Serial.println( "Altitude ( m ): " + String( bmp.readAltitude( SEALEVELPRESSURE_HPA ) ) + '\n' );
+  Serial.println( output ); // Print output to screen
 
   Serial.end(); // End Serial Transmission
 

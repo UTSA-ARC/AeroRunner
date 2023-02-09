cff83614ed4b5a1533a972ae31b1e1ff7e0c7420
eliteabhi <abhirangarajan2003@gmail.com>
2023-02-08 19:52:04 -0600

    Clean up!

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
-/*
-   Arduino and MPU6050 Accelerometer and Gyroscope Sensor Tutorial
-   by Dejan, https://howtomechatronics.com
-*/
+/**
+ *
+ * @file main.cpp
+ * @author UTSA ARC Avionics Team 2023
+ * @brief The Avionics code for the Spaceport 2023 vehicle
+ * @version 1.0.0
+ * @date 2022-2023
+ * 
+ */
 
-#include "setup.h"
+#include "functions.h"
 
 void setup() {
   // Find hexadecimal representation of accelerometer range based on decimal global variable AccelRange defined above // 
@@ -12,21 +17,36 @@ void setup() {
   Serial.begin( 115200 ); //Changed to higher rate 4/21/22
 
   while ( !Serial );
-  
+
+  // ----------------------------------------------------------------
+
   // Builtin SD Card Initialization 
   Serial.print("Initializing SD card...");
 
-  while ( !SD.begin(BUILTIN_SDCARD) )
-    Serial.println("Card not readable");
+  while ( !SD.begin( BUILTIN_SDCARD ) ) {
 
-  Serial.println("Card initialized");
+    Serial.println( "Card not readable" );
+    delay( 2000 );
 
+  }
+
+  Serial.println( "SD card is ready!" );
+
+  // ----------------------------------------------------------------
+
+  Serial.println( "Initializing BMP3..." );
   while ( !bmp.begin_I2C() ) { // hardware I2C mode, can pass in address & alt Wire
 
     Serial.println( "Could not find a valid BMP3 sensor, check wiring!\n" );
     delay( 2000 );
     
   }
+
+  Serial.println( "Found and initialized a valid BMP3 I2C sensor!" );
+
+  // ----------------------------------------------------------------
+
+  Serial.println( "Initializing MPU6050..." );
 
   Wire.beginTransmission( MPU );
   while ( ( Wire.endTransmission() != 0 ) ) {
@@ -36,16 +56,23 @@ void setup() {
     
   }
 
-  // Set up oversampling and filter initialization
+  // ----------------------------------------------------------------
 
+  // Set up oversampling and filter initialization
   bmp.setTemperatureOversampling( BMP3_OVERSAMPLING_8X );
   bmp.setPressureOversampling( BMP3_OVERSAMPLING_4X );
   bmp.setIIRFilterCoeff( BMP3_IIR_FILTER_COEFF_3 );
   bmp.setOutputDataRate( BMP3_ODR_50_HZ );
 
+  // ----------------------------------------------------------------
+
+  // Pin configuration
   pinMode( pinCS, OUTPUT );
   pinMode( pinMOSI, OUTPUT );
 
+  // ----------------------------------------------------------------
+
+  // Set Ranges
   while ( Set_Accel_Range( AccelRange ) != 0 ) {
     
     Serial.println( "Please Fix Accel Range" );
@@ -60,63 +87,31 @@ void setup() {
 
   }
 
+  // ----------------------------------------------------------------
+
   // Find hexadecimal representation of gyroscope range based on decimal global variable GyroRange defined above
   // Find decimal representation of LSB Sensitivity based on decimal global variable GyroRange defined above
 
-  myFile.print( "Time ( seconds ),Raw Ax ( g ),Ax ( g ),Raw Ay ( g ),Ay ( g ),Raw Az ( g ),Az ( g ),Raw Gx ( deg/s ),Gx ( deg/s ),Raw Gy ( deg/s ),Gy ( deg/s ),Raw Gz ( deg/s ),Gz ( deg/s ), Temperature ( *C ), Pressure ( kpA ), Altitude ( m )" );
-  myFile.print( "Time,RAx,Ax,RAy,Ay,RAz,Az,RGx,Gx,RGy,Gy,RGz,Gz, Temp ( *C ), P ( kPa ), Alt ( m )" );
+  Init_MPU();            // Initialize MPU
+  Configure_MPU( 0x1C ); // Config Register
  
-  myFile.println();
-  myFile.close();
+  Configure_Gyro( 0x1B ); // Config Register
 
-  Serial.end();
+  // ----------------------------------------------------------------
 
-  delay( 20 );
+  Init_CSV(); // Initialize CSV
+
+  Serial.end(); // End Serial Transmission
+
+  delay( 20 ); // Delay for 20 Milliseconds before starting main loop
 
 }
 
 void loop() {
 
-  Serial.begin( 115200 );
-  while ( !Serial );
-  
-  // === Read acceleromter data === //
-  
-  Vector<int> raw_accel = Get_Raw_Accel();
-  Vector<int> accel = Normalized_Accel( raw_accel );
-
-  // === Read gyroscope data === //
-
-  const Vector<int> raw_gyro = Get_Raw_Gyro();
-  const Vector<int> gyro = Normalized_Gyro( raw_gyro );
- 
-  // Print the values on the serial monitor
-
-  char c = ',';
-
-  Serial.println( "Time ( S ): " + String( millis() / 1000.0 ) );
-
-  Serial.println( "Raw Acceleration ( X, Y, Z ): " + String( raw_accel.at( 0 ) / 1000.0, 2 ) + c + String( raw_accel.at( 1 ) / 1000.0, 2 ) + c + String( raw_accel.at( 0 )  / 1000.0, 2 ) ); // Arduino is really dumb
-  
-  Serial.println( "Normalized Acceleration ( X, Y, Z ): " + String( accel.at( 0 )  / 1000.0, 2 ) + c + String( accel.at( 1 ) / 1000.0, 2 ) + c + String( accel.at( 2 ) / 1000.0, 2 ) ); // Arduino is really dumb again
-  
-  Serial.println( "Raw GyroRange ( X, Y, Z ): " + String( raw_gyro.at( 0 ) / 1000.0, 2 ) + c + String( raw_gyro.at( 1 ) / 1000.0, 2 ) + c + String( raw_gyro.at( 2 ) / 1000.0, 2 ) ); // Arduino is really dumb again,...again
-
-  Serial.println( "Normalized Gyro Range ( X, Y, Z ): " + String( gyro.at( 0 ) / 1000.0, 2 ) + c + String( gyro.at( 1 ) / 1000.0, 2 ) + c + String( gyro.at( 2 ) / 1000.0, 2 ) + '\n' ); // Arduino is really dumb again,...again, and again
-
-  Serial.println( "Now reading BMP390..." );
-  
-  Serial.println( "Tempurature ( C ): " + String( bmp.temperature ) );
-  
-  Serial.println( "Pressure ( kPa ): " + String( bmp.pressure / 1000.0 ) );
-
-  Serial.println( "Altitude ( m ): " + String( bmp.readAltitude( SEALEVELPRESSURE_HPA ) ) + '\n' );
-
-  Serial.end();
-
-  // myFile = SD.open( "Raw_V05.csv", FILE_WRITE );
-  // myFile.println( String( bmp.temperature ) + c + String( bmp.pressure / 1000.0 ) + c + String( bmp.readAltitude( SEALEVELPRESSURE_HPA ) ) + '\n' );
-  // myFile.close();
+  // Print & Save All Values
+  Record_Data();
 
   delay( 2000 );
+
 }

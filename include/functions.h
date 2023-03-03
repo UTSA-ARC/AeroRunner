acb1feadc033cf1f6bedfa6b9f3c8c52ba95bfb1
eliteabhi <abhirangarajan2003@gmail.com>
2023-03-03 15:28:59 -0600

    Implimented `Check_Altitude()` & Reformated to 4 space tabs

--- include/functions.h
+++ include/functions.h
@@ -5,51 +5,51 @@
 
 int* Get_Raw_Accel() { // Returns an int vector of the raw acceleration Values from the MPU
 
-  Wire.beginTransmission( MPU );
-  Wire.write( 0x3B );                   // Start with register 0x3B ( ACCEL_XOUT_H )
-  Wire.endTransmission( false );
-  Wire.requestFrom( MPU, 6, true );     // Read 6 registers total, each axis value is stored in 2 registers
-   
-  static int result[ 3 ];
+    Wire.beginTransmission( MPU );
+    Wire.write( 0x3B );                       // Start with register 0x3B ( ACCEL_XOUT_H )
+    Wire.endTransmission( false );
+    Wire.requestFrom( MPU, 6, true );         // Read 6 registers total, each axis value is stored in 2 registers
 
-  for ( int i = 0; i < 3; i++ ) result[ i ] = ( ( Wire.read() << 8 | Wire.read() ) * 1000 ); // Raw values
+    static int result[ 3 ];
 
-  return result;
+    for ( int i = 0; i < 3; i++ ) result[ i ] = ( ( Wire.read() << 8 | Wire.read() ) * 1000 ); // Raw values
+
+    return result;
 
 }
 
 int* Get_Normalized_Accel( int* raw_accel ) { // Returns the normalized acceleration Values from the MPU
-  
-  static int normalized_accel[ 3 ];
 
-  for ( int i = 0; i < 3; i++ ) normalized_accel[ i ] = raw_accel[ i ] / ALSB_Sensitivity ;
+    static int normalized_accel[ 3 ];
 
-  return normalized_accel;
+    for ( int i = 0; i < 3; i++ ) normalized_accel[ i ] = raw_accel[ i ] / ALSB_Sensitivity ;
+
+    return normalized_accel;
 
 }
 
 int* Get_Raw_Gyro() { // Returns an int vector containing the raw gyrospocic Values from the MPU
 
-  Wire.beginTransmission( MPU );
-  Wire.write( 0x43 );                 // Gyro data first register address 0x43
-  Wire.endTransmission( false );
-  Wire.requestFrom( MPU, 6, true );   // Read 4 registers total, each axis value is stored in 2 registers
+    Wire.beginTransmission( MPU );
+    Wire.write( 0x43 );                     // Gyro data first register address 0x43
+    Wire.endTransmission( false );
+    Wire.requestFrom( MPU, 6, true );       // Read 4 registers total, each axis value is stored in 2 registers
 
-  static int raw_gyro[ 3 ];
-  
-  for ( int i = 0; i < 3; i++ ) raw_gyro[ i ] = ( Wire.read() << 8 | Wire.read() ) * 1000;
+    static int raw_gyro[ 3 ];
 
-  return raw_gyro;
+    for ( int i = 0; i < 3; i++ ) raw_gyro[ i ] = ( Wire.read() << 8 | Wire.read() ) * 1000;
+
+    return raw_gyro;
 
 }
 
 int* Get_Normalized_Gyro( int* raw_gyro ) { // Returns an int vector containing the normalized gyrospocic Values from the MPU
 
-  static int normalized_gyro[ 3 ];
+    static int normalized_gyro[ 3 ];
 
-  for ( int i = 0; i < 3; i++ ) normalized_gyro[ i ] = raw_gyro[ 0 ] / GLSB_Sensitivity;
+    for ( int i = 0; i < 3; i++ ) normalized_gyro[ i ] = raw_gyro[ 0 ] / GLSB_Sensitivity;
 
-  return normalized_gyro;
+    return normalized_gyro;
 
 }
 
@@ -57,38 +57,38 @@ int* Get_Normalized_Gyro( int* raw_gyro ) { // Returns an int vector containing 
 
 INTData Get_All_Values_INT() {
 
-  INTData data;
+    INTData data;
 
-  int i = 0;
-  // Get Values from Accelorometer
-  int *raw_a_ptr = Get_Raw_Accel();
-  int *raw_g_ptr = Get_Raw_Gyro();
-  int *norm_a_ptr = Get_Normalized_Accel( raw_a_ptr );
-  int *norm_g_ptr = Get_Normalized_Gyro( raw_g_ptr );
+    int i = 0;
+    // Get Values from Accelorometer
+    int *raw_a_ptr = Get_Raw_Accel();
+    int *raw_g_ptr = Get_Raw_Gyro();
+    int *norm_a_ptr = Get_Normalized_Accel( raw_a_ptr );
+    int *norm_g_ptr = Get_Normalized_Gyro( raw_g_ptr );
 
-  for ( i = 0; i < 3; i++ ) {
+    for ( i = 0; i < 3; i++ ) {
 
-    data.raw_accel[ i ] = raw_a_ptr[ i ];
-    data.raw_gyro[ i ] = raw_g_ptr[ i ];
-    data.normalized_accel[ i ] = norm_a_ptr[ i ];
-    data.normalized_gyro[ i ] = norm_g_ptr[ i ];
+        data.raw_accel[ i ] = raw_a_ptr[ i ];
+        data.raw_gyro[ i ] = raw_g_ptr[ i ];
+        data.normalized_accel[ i ] = norm_a_ptr[ i ];
+        data.normalized_gyro[ i ] = norm_g_ptr[ i ];
 
-  }
+    }
 
-  time_t time_now = now();
-  data.time = (
+    time_t time_now = now();
+    data.time = (
 
-    String( hour( time_now ) ) + ":" + 
-    String( minute( time_now ) < 10 ? ( '0' + String( minute( time_now ) ) ) : minute( time_now ) ) + ":" + 
-    String( second( time_now ) < 10 ? ( '0' + String( second( time_now ) ) ) : second( time_now ) ) 
-    
-    );
+        String( hour( time_now ) ) + ":" +
+        String( minute( time_now ) < 10 ? ( '0' + String( minute( time_now ) ) ) : minute( time_now ) ) + ":" +
+        String( second( time_now ) < 10 ? ( '0' + String( second( time_now ) ) ) : second( time_now ) )
 
-  data.temperature = static_cast<int>( bmp.temperature * 1000 );
-  data.pressure = static_cast<int>( bmp.pressure * 1000 );
-  data.altitude = static_cast<int>( bmp.readAltitude( SEALEVELPRESSURE_HPA ) * 1000 );
+        );
 
-  return data;
+    data.temperature = static_cast<int>( bmp.temperature * 1000 );
+    data.pressure = static_cast<int>( bmp.pressure * 1000 );
+    data.altitude = static_cast<int>( bmp.readAltitude( SEALEVELPRESSURE_HPA ) * 1000 );
+
+    return data;
 
 }
 
@@ -96,65 +96,103 @@ INTData Get_All_Values_INT() {
 
 void Print_All_Values( INTData& Values ) { // Print the Values on the serial monitor
 
-  Serial.begin( 115200 ); // Open Serial Port
+    Serial.begin( 115200 ); // Open Serial Port
 
-  String output = ""; // init output string
+    String output = ""; // init output string
 
-  output.append( "Time ( S ): " + Values.time + '\n' );
-  output.append( "Raw Acceleration ( X, Y, Z ): " + String( Values.raw_accel[ 0 ] / 1000.0f, 2 ) + ',' + String( Values.raw_accel[ 1 ] / 1000.0f, 2 ) + ',' + String( Values.raw_accel[ 2 ]  / 1000.0f, 2 ) + '\n' );
-  output.append( "Normalized Acceleration ( X, Y, Z ): " + String( Values.normalized_accel[ 0 ] / 1000.0f, 2 ) + ',' + String( Values.normalized_accel[ 1 ] / 1000.0f, 2 ) + ',' + String( Values.normalized_accel[ 2 ] / 1000.0f, 2 ) + '\n' );
-  output.append( "Raw GyroRange ( X, Y, Z ): " + String( Values.raw_gyro[ 0 ] / 1000.0f, 2 ) + ',' + String( Values.raw_gyro[ 1 ] / 1000.0f, 2 ) + ',' + String( Values.raw_gyro[ 2 ] / 1000.0f, 2 ) + '\n' );
-  output.append( "Normalized Gyro Range ( X, Y, Z ): " + String( Values.normalized_gyro[ 0 ] / 1000.0f, 2 ) + ',' + String( Values.normalized_gyro[ 1 ] / 1000.0f, 2 ) + ',' + String( Values.normalized_gyro[ 2 ] / 1000.0f, 2 ) + '\n' );
+    output.append( "Time ( S ): " + Values.time + '\n' );
 
-  output.append( "\n\nNow reading BMP390...\n" );
-  output.append( "Tempurature ( C ): " + String( Values.temperature / 1000.0f ) + '\n' );
-  output.append( "Pressure ( kPa ): " + String( Values.pressure / 1000.0f ) + '\n' );
-  output.append( "Altitude ( m ): " + String( Values.altitude / 1000.0f ) + '\n' );
+    output.append(
 
-  Serial.println( output ); // Print output to screen
+        "Raw Acceleration ( X, Y, Z ): " +
+        String( Values.raw_accel[ 0 ] / 1000.0f, 2 ) + ',' +
+        String( Values.raw_accel[ 1 ] / 1000.0f, 2 ) + ',' +
+        String( Values.raw_accel[ 2 ]    / 1000.0f, 2 ) + '\n'
+        
+        );
 
-  Serial.end(); // End Serial Transmission
+    output.append( 
+        
+        "Normalized Acceleration ( X, Y, Z ): " +
+        String( Values.normalized_accel[ 0 ] / 1000.0f, 2 ) + ',' +
+        String( Values.normalized_accel[ 1 ] / 1000.0f, 2 ) + ',' +
+        String( Values.normalized_accel[ 2 ] / 1000.0f, 2 ) + '\n'
+        
+        );
+
+    output.append(
+
+        "Raw GyroRange ( X, Y, Z ): " +
+        String( Values.raw_gyro[ 0 ] / 1000.0f, 2 ) + ',' +
+        String( Values.raw_gyro[ 1 ] / 1000.0f, 2 ) + ',' +
+        String( Values.raw_gyro[ 2 ] / 1000.0f, 2 ) + '\n'
+
+        );
+
+    output.append(
+
+        "Normalized Gyro Range ( X, Y, Z ): " +
+        String( Values.normalized_gyro[ 0 ] / 1000.0f, 2 ) + ',' +
+        String( Values.normalized_gyro[ 1 ] / 1000.0f, 2 ) + ',' +
+        String( Values.normalized_gyro[ 2 ] / 1000.0f, 2 ) + '\n'
+        
+        );
+
+    output.append( "\n\nNow reading BMP390...\n" );
+    output.append( "Tempurature ( C ): " + String( Values.temperature / 1000.0f ) + '\n' );
+    output.append( "Pressure ( kPa ): " + String( Values.pressure / 1000.0f ) + '\n' );
+    output.append( "Altitude ( m ): " + String( Values.altitude / 1000.0f ) + '\n' );
+
+    Serial.println( output ); // Print output to screen
+
+    Serial.end(); // End Serial Transmission
 
 }
 
 void Write_All_Values_To_SD( INTData& Values ) { // Records values to Sd card
 
-  File myFile = SD.open( "Raw_V05.csv", FILE_WRITE );
+    File myFile = SD.open( "Raw_V05.csv", FILE_WRITE );
 
-  //* Time ( seconds ),Raw Ax ( g ),Raw Ay ( g ),Raw Az ( g ),Ax ( g ),Ay ( g ),Az ( g ),Raw Gx ( deg/s ),Raw Gy ( deg/s ),Raw Gz ( deg/s ),Gx ( deg/s ),Gy ( deg/s ),Gz ( deg/s ),Temperature ( *C ),Pressure ( kpA ),Altitude ( m ),Message
+    //* Time ( seconds ),
+    //* Raw Ax ( g ),Raw Ay ( g ),Raw Az ( g ),
+    //* Ax ( g ),Ay ( g ),Az ( g ),
+    //* Raw Gx ( deg/s ),Raw Gy ( deg/s ),Raw Gz ( deg/s ),
+    //* Gx ( deg/s ),Gy ( deg/s ),Gz ( deg/s ),
+    //* Temperature ( *C ),Pressure ( kpA ),Altitude ( m ),
+    //* Message
 
-  String output = "";
+    String output = "";
 
-  output += ( Values.time + ',' );
+    output += ( Values.time + ',' );
 
-  int i;
+    int i;
 
-  for ( i = 0; i < 3; i++ ) output += ( String( Values.raw_accel[ i ] / 1000.0f ) + ',' );
+    for ( i = 0; i < 3; i++ ) output += ( String( Values.raw_accel[ i ] / 1000.0f ) + ',' );
 
-  for ( i = 0; i < 3; i++ ) output += ( String( Values.normalized_accel[ i ] / 1000.0f ) + ',' );
+    for ( i = 0; i < 3; i++ ) output += ( String( Values.normalized_accel[ i ] / 1000.0f ) + ',' );
 
-  for ( i = 0; i < 3; i++ ) output += ( String( Values.raw_gyro[ i ] / 1000.0f ) + ',' );
+    for ( i = 0; i < 3; i++ ) output += ( String( Values.raw_gyro[ i ] / 1000.0f ) + ',' );
 
-  for ( i = 0; i < 3; i++ ) output += ( String( Values.normalized_gyro[ i ] / 1000.0f ) + ',' );
+    for ( i = 0; i < 3; i++ ) output += ( String( Values.normalized_gyro[ i ] / 1000.0f ) + ',' );
 
-  output += ( String( Values.temperature / 1000.0f ) + ',' );
+    output += ( String( Values.temperature / 1000.0f ) + ',' );
 
-  output += ( String( Values.pressure / 1000.0f ) + ',' );
+    output += ( String( Values.pressure / 1000.0f ) + ',' );
 
-  output += ( String( Values.altitude / 1000.0f ) + ',' );
+    output += ( String( Values.altitude / 1000.0f ) + ',' );
 
-  output += ( Values.message );
+    output += ( Values.message );
 
-  myFile.println( output );
+    myFile.println( output );
 
-  myFile.close();
+    myFile.close();
 
 }
 
 void Record_Data( INTData& Values ) { // Prints data to screen and saves it to file
 
-  Print_All_Values( Values );
-  Write_All_Values_To_SD( Values );
+    Print_All_Values( Values );
+    Write_All_Values_To_SD( Values );
 
 }
 
@@ -162,55 +200,57 @@ void Record_Data( INTData& Values ) { // Prints data to screen and saves it to f
 
 void Deploy_Parachute( int pin ) { // Arms Parachute
 
-  Para_Armed = 1;
+    Para_Armed = 1;
 
 }
 
 void Launch_Parachute( int schute ) { // Launches Parachute
 
-  switch ( schute ) {
+    switch ( schute ) {
 
-    case 0: // Main Schute
-      digitalWrite( pinMain, HIGH );
-    
-    case 1: // Drouge Schute
-      digitalWrite( pinDrouge, HIGH );
+        case 0: // Main Schute
+            digitalWrite( pinMain, HIGH );
 
-  }
+        case 1: // Drouge Schute
+            digitalWrite( pinDrouge, HIGH );
+
+    }
 
 }
 
 // -----------------------Internal Trigger Functions--------------------------------
 
-int Check_Altitude( int altitude ) { // Checks if altitude is safe
+Result Check_Altitude( int altitude, int prev_altitude = -1 ) { // Checks if altitude is safe/at apogee
 
-  //TODO: Implement
+ if ( altitude > safeAltitude )
+    if ( altitude <= prev_altitude ) return { 2, "!!AT APOGEE!!" };
+    else return { 0, "SAFE ALTITUDE" };
 
-  return 0; // Safe
+    return { -1, "-" }; // Safe
 
 }
 
 int Check_Pressure( int pressure ) { // Checks if pressure is safe
 
-  //TODO: Implement
+    //TODO: Implement
 
-  return 0; // Safe
+    return 0; // Safe
 
 }
 
 int Check_Tilt( int gyro ) { // Checks if tilt is safe
 
-  //TODO: Implement
+    //TODO: Implement
 
-  return 0; // Safe
+    return 0; // Safe
 
 }
 
 int Check_Accel( int accel ) { // Checks if accel is correct
 
-  //TODO: Implement
+    //TODO: Implement
 
-  return 0; // Safe
+    return 0; // Safe
 
 }
 
@@ -218,10 +258,10 @@ int Check_Accel( int accel ) { // Checks if accel is correct
 
 int Check_Systems( INTData& Values ) { // Checks if systems are safe
 
-  //TODO: Implement
-  
-  //* Will trigger LED based on error code
+    //TODO: Implement
 
-  return 0; // Safe
+    //* Will trigger LED based on error code
+
+    return 0; // Safe
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -11,108 +11,108 @@
 #include "functions.h"
 
 void setup() {
-  // Find hexadecimal representation of accelerometer range based on decimal global variable AccelRange defined above // 
-  // Find decimal representation of LSB Sensitivity based on decimal global variable AccelRange defined above // 
+    // Find hexadecimal representation of accelerometer range based on decimal global variable AccelRange defined above // 
+    // Find decimal representation of LSB Sensitivity based on decimal global variable AccelRange defined above // 
 
-  Serial.begin( 115200 ); //Changed to higher rate 4/21/22
+    Serial.begin( 115200 ); //Changed to higher rate 4/21/22
 
-  while ( !Serial ); //! DELETE WHEN PUTTING IN ROCKET
+    while ( !Serial ); //! DELETE WHEN PUTTING IN ROCKET
 
-  // ----------------------------------------------------------------
+    // ----------------------------------------------------------------
 
-  // Builtin SD Card Initialization 
-  Serial.print("Initializing SD card...");
+    // Builtin SD Card Initialization 
+    Serial.print("Initializing SD card...");
 
-  while ( !SD.begin( BUILTIN_SDCARD ) ) {
+    while ( !SD.begin( BUILTIN_SDCARD ) ) {
 
-    Serial.println( "Card not readable" );
-    delay( 2000 );
-
-  }
-
-  Serial.println( "SD card is ready!" );
-
-  // ----------------------------------------------------------------
-
-  Serial.println( "Initializing BMP3..." );
-  while ( !bmp.begin_I2C() ) { // hardware I2C mode, can pass in address & alt Wire
-
-    Serial.println( "Could not find a valid BMP3 sensor, check wiring!\n" );
-    delay( 2000 );
-    
-  }
-
-  Serial.println( "Found and initialized a valid BMP3 I2C sensor!" );
-
-  // ----------------------------------------------------------------
-
-  Serial.println( "Initializing MPU6050..." );
-
-  Wire.beginTransmission( MPU );
-  while ( ( Wire.endTransmission() != 0 ) ) {
-
-    Serial.println( "Could not find MPU\n" );
-    delay( 2000 );
-    
-  }
-
-  Serial.println( "Initialized MPU6050! ");
-
-  // ----------------------------------------------------------------
-
-  // Set up oversampling and filter initialization
-  bmp.setTemperatureOversampling( BMP3_OVERSAMPLING_8X );
-  bmp.setPressureOversampling( BMP3_OVERSAMPLING_4X );
-  bmp.setIIRFilterCoeff( BMP3_IIR_FILTER_COEFF_3 );
-  bmp.setOutputDataRate( BMP3_ODR_50_HZ );
-
-  // ----------------------------------------------------------------
-
-  // Pin configuration
-  pinMode( pinCS, OUTPUT );
-  pinMode( pinMOSI, OUTPUT );
-
-  // ----------------------------------------------------------------
-
-  // Set Ranges
-  while ( Set_Accel_Range( AccelRange ) != 0 ) {
-    
-    Serial.println( "Please Fix Accel Range" );
-    delay( 2000 );
+        Serial.println( "Card not readable" );
+        delay( 2000 );
 
     }
-  
-  while ( Set_Gyro_Range( GyroRange ) != 0 ) {
 
-    Serial.println( "Please Fix Gyro Range" );
-    delay( 2000 );
+    Serial.println( "SD card is ready!" );
 
-  }
+    // ----------------------------------------------------------------
 
-  // ----------------------------------------------------------------
+    Serial.println( "Initializing BMP3..." );
+    while ( !bmp.begin_I2C() ) { // hardware I2C mode, can pass in address & alt Wire
 
-  Init_MPU();            // Initialize MPU
-  Configure_MPU( 0x1C ); // Config Register
+        Serial.println( "Could not find a valid BMP3 sensor, check wiring!\n" );
+        delay( 2000 );
+        
+    }
+
+    Serial.println( "Found and initialized a valid BMP3 I2C sensor!" );
+
+    // ----------------------------------------------------------------
+
+    Serial.println( "Initializing MPU6050..." );
+
+    Wire.beginTransmission( MPU );
+    while ( ( Wire.endTransmission() != 0 ) ) {
+
+        Serial.println( "Could not find MPU\n" );
+        delay( 2000 );
+        
+    }
+
+    Serial.println( "Initialized MPU6050! ");
+
+    // ----------------------------------------------------------------
+
+    // Set up oversampling and filter initialization
+    bmp.setTemperatureOversampling( BMP3_OVERSAMPLING_8X );
+    bmp.setPressureOversampling( BMP3_OVERSAMPLING_4X );
+    bmp.setIIRFilterCoeff( BMP3_IIR_FILTER_COEFF_3 );
+    bmp.setOutputDataRate( BMP3_ODR_50_HZ );
+
+    // ----------------------------------------------------------------
+
+    // Pin configuration
+    pinMode( pinCS, OUTPUT );
+    pinMode( pinMOSI, OUTPUT );
+
+    // ----------------------------------------------------------------
+
+    // Set Ranges
+    while ( Set_Accel_Range( AccelRange ) != 0 ) {
+        
+        Serial.println( "Please Fix Accel Range" );
+        delay( 2000 );
+
+        }
+    
+    while ( Set_Gyro_Range( GyroRange ) != 0 ) {
+
+        Serial.println( "Please Fix Gyro Range" );
+        delay( 2000 );
+
+    }
+
+    // ----------------------------------------------------------------
+
+    Init_MPU();                        // Initialize MPU
+    Configure_MPU( 0x1C ); // Config Register
  
-  Configure_Gyro( 0x1B ); // Config Register
+    Configure_Gyro( 0x1B ); // Config Register
 
-  // ----------------------------------------------------------------
+    // ----------------------------------------------------------------
 
-  Init_CSV(); // Initialize CSV
+    Init_CSV(); // Initialize CSV
 
-  Serial.end(); // End Serial Transmission
+    Serial.end(); // End Serial Transmission
 
-  delay( 20 ); // Delay for 20 Milliseconds before starting main loop
+    delay( 20 ); // Delay for 20 Milliseconds before starting main loop
 
 }
 
 void loop() {
 
-  INTData values = Get_All_Values_INT(); // Get all data values
+    INTData values = Get_All_Values_INT(); // Get all data values
 
-  
+    
 
-  // Print & Save All Values
-  Record_Data( values );
-  delay(1000);
+    // Print & Save All Values
+    Record_Data( values );
+    delay(1000);
 }

93d1d14dbec70f01b9c0a96339a448f193cf65cc
juan00juan0 <jherrera2343@gmail.com>
2023-12-28 00:33:30 -0600

    added smoothing filter

--- include/samples.h
+++ include/samples.h
@@ -8,6 +8,7 @@ class Sample { // An Average of Measurements
 
         Data measurements[ MeasurementAmount ]; // An array of measurements
         SampleData avg_data; // Avg of measurements
+        SampleData filtered_data; // Exponential smoothing of measurements
 
         void Set_Measurements( const Data* new_measurements ) { *measurements = *new_measurements; } // Set measurements
 
@@ -47,6 +48,42 @@ class Sample { // An Average of Measurements
 
         }
 
+        void Find_Filtered_Data() { // Runs data through an exponential smoothing filter
+
+            filtered_data.message = "";
+            filtered_data.time = measurements[0].time;
+            filtered_data.timeEnd = measurements[MeasurementAmount -  1].time;
+
+            for ( int i = 0; i < MeasurementAmount; i++ ) {
+
+                filtered_data.raw_accel[0] = Filter_Weight * measurements[i].raw_accel[0] + (1 - Filter_Weight) * filtered_data.raw_accel[0];
+                filtered_data.raw_accel[1] = Filter_Weight * measurements[i].raw_accel[1] + (1 - Filter_Weight) * filtered_data.raw_accel[1];
+                filtered_data.raw_accel[2] = Filter_Weight * measurements[i].raw_accel[2] + (1 - Filter_Weight) * filtered_data.raw_accel[2];
+
+                filtered_data.raw_gyro[0] = Filter_Weight * measurements[i].raw_gyro[0] + (1 - Filter_Weight) * filtered_data.raw_gyro[0];
+                filtered_data.raw_gyro[1] = Filter_Weight * measurements[i].raw_gyro[1] + (1 - Filter_Weight) * filtered_data.raw_gyro[1];
+                filtered_data.raw_gyro[2] = Filter_Weight * measurements[i].raw_gyro[2] + (1 - Filter_Weight) * filtered_data.raw_gyro[2];
+
+                filtered_data.normalized_accel[0] = Filter_Weight * measurements[i].normalized_accel[0] + (1 - Filter_Weight) * filtered_data.normalized_accel[0];
+                filtered_data.normalized_accel[1] = Filter_Weight * measurements[i].normalized_accel[1] + (1 - Filter_Weight) * filtered_data.normalized_accel[1];
+                filtered_data.normalized_accel[2] = Filter_Weight * measurements[i].normalized_accel[2] + (1 - Filter_Weight) * filtered_data.normalized_accel[2];
+
+                filtered_data.normalized_gyro[0] = Filter_Weight * measurements[i].normalized_gyro[0] + (1 - Filter_Weight) * filtered_data.normalized_gyro[0];
+                filtered_data.normalized_gyro[1] = Filter_Weight * measurements[i].normalized_gyro[1] + (1 - Filter_Weight) * filtered_data.normalized_gyro[1];
+                filtered_data.normalized_gyro[2] = Filter_Weight * measurements[i].normalized_gyro[2] + (1 - Filter_Weight) * filtered_data.normalized_gyro[2];
+
+                filtered_data.altitude = Filter_Weight * measurements[i].altitude + (1 - Filter_Weight) * filtered_data.altitude;
+
+                filtered_data.pressure = Filter_Weight * measurements[i].pressure + (1 - Filter_Weight) * filtered_data.pressure;
+
+                filtered_data.temperature = Filter_Weight * measurements[i].temperature + (1 - Filter_Weight) * filtered_data.temperature;
+
+                filtered_data.message += measurements[i].message;
+
+            }
+
+        }
+
     public:
 
         Sample() { // Initialize Sample object
@@ -73,6 +110,8 @@ class Sample { // An Average of Measurements
         Data* Get_Measurements() { return measurements; } // Get measurements array
 
         SampleData Get_Avg_Data() { return avg_data; } // Get Avg_data
+
+        SampleData Get_Filtered_Data() {return filtered_data; } // Get filtered data
 
         void Append_Message( const String message ) { avg_data.message += message;  } // Append to message string of Sample object
 
